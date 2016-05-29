4e8747fbbede00db803a3587d96f5b83f0a5381b
Kingdaro <kingdaro@gmail.com>
2016-05-28 23:57:12 -0400

    added a nicer appearance to mines

--- game/mine.lua
+++ game/mine.lua
@@ -1,7 +1,11 @@
 local util = require 'util'
 
 local mine = {}
-local size = 40
+
+local innerSize = 25
+local outerSize = 12
+local outerDistance = 40
+local outerCount = 3
 
 function mine.new(x, y)
   local self = setmetatable({}, { __index = mine })
@@ -15,12 +19,28 @@ function mine:update(dt)
 end
 
 function mine:draw()
+  love.graphics.push('all')
+
   love.graphics.setColor(util.toLoveColor(0.8, 0.3, 0.3))
-  love.graphics.push()
   love.graphics.translate(self.x, self.y)
-  love.graphics.rotate(math.rad(self.y))
-  love.graphics.translate(-size / 2, -size / 2)
-  love.graphics.rectangle('fill', 0, 0, size, size)
+
+
+  for i = 1, outerCount do
+    love.graphics.push()
+    love.graphics.rotate(math.rad(i * (360 / outerCount) - self.y / 4))
+    love.graphics.translate(outerDistance, 0)
+
+    love.graphics.push()
+    love.graphics.rotate(-math.rad(self.y / 2))
+    love.graphics.rectangle('fill', -outerSize / 2, -outerSize / 2, outerSize, outerSize)
+    love.graphics.pop()
+
+    love.graphics.pop()
+  end
+
+  love.graphics.rotate(math.rad(self.y / 2))
+  love.graphics.rectangle('fill', -innerSize / 2, -innerSize / 2, innerSize, innerSize)
+
   love.graphics.pop()
 end
 
