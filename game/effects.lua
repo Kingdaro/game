3b2fa95f36932140b789e017442279ad5d1ed74e
Kingdaro <kingdaro@gmail.com>
2016-05-29 03:47:24 -0400

    split effect logic into separate function

---
+++ game/effects.lua
@@ -0,0 +1,38 @@
+local util = require "util"
+
+local effects = {}
+
+function effects:init(clock, music)
+  self.shake = { x = 0, y = 0 }
+  self.clock = clock
+
+  self.clock:schedule(function(wait)
+    wait(2)
+    music:play()
+    wait(0.1)
+    while true do
+      self.shake.x = 20
+      wait(60 / 130)
+      self.shake.y = 20
+      wait(60 / 130)
+    end
+  end)
+end
+
+function effects:update(dt)
+  self.shake.x = util.interpolate(self.shake.x, 0, dt * 12)
+  self.shake.y = util.interpolate(self.shake.y, 0, dt * 12)
+end
+
+function effects:transform(draw)
+  local sx = love.math.random(-self.shake.x, self.shake.x)
+  local sy = love.math.random(-self.shake.y, self.shake.y)
+
+  love.graphics.push()
+  love.graphics.translate(sx, sy)
+
+  draw()
+  love.graphics.pop()
+end
+
+return effects
--- game/gameplay.lua
+++ game/gameplay.lua
@@ -3,26 +3,15 @@ local player = require "player"
 local input = require "input"
 local clock = require "clock"
 local util = require "util"
+local effects = require "effects"
 
 local gameplay = {}
 
 function gameplay:init()
   self.music = love.audio.newSource('fragments of darkness.mp3')
   self.clock = clock.new()
-  self.shake = { x = 0, y = 0 }
 
-  self.clock:schedule(function(wait)
-    wait(2)
-    self.music:play()
-    wait(0.1)
-    while true do
-      self.shake.x = 20
-      wait(60 / 130)
-      self.shake.y = 20
-      wait(60 / 130)
-    end
-  end)
-
+  effects:init(self.clock, self.music)
   field:init(3, self.clock)
   player:init(self.clock)
 end
@@ -30,9 +19,7 @@ end
 function gameplay:update(dt)
   player:update(dt)
   field:update(dt)
-
-  self.shake.x = util.interpolate(self.shake.x, 0, dt * 12)
-  self.shake.y = util.interpolate(self.shake.y, 0, dt * 12)
+  effects:update(dt)
 end
 
 function gameplay:keypressed(key)
@@ -44,17 +31,11 @@ function gameplay:keypressed(key)
 end
 
 function gameplay:draw()
-  local sx = love.math.random(-self.shake.x, self.shake.x)
-  local sy = love.math.random(-self.shake.y, self.shake.y)
-
-  love.graphics.push()
-  love.graphics.translate(sx, sy)
-
-  field:drawBackground()
-  field:drawMines()
-  player:draw()
-
-  love.graphics.pop()
+  effects:transform(function()
+    field:drawBackground()
+    field:drawMines()
+    player:draw()
+  end)
 end
 
 return gameplay
