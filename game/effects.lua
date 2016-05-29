e8122684aab39bd09cc344413540867b4599fc58
Kingdaro <kingdaro@gmail.com>
2016-05-29 03:54:48 -0400

    base shake intensity on 0-1

--- game/effects.lua
+++ game/effects.lua
@@ -11,21 +11,21 @@ function effects:init(clock, music)
     music:play()
     wait(0.1)
     while true do
-      self.shake.x = 15
-      self.shake.y = 15
+      self.shake.x = 1
+      self.shake.y = 1
       wait(60 / 130)
     end
   end)
 end
 
 function effects:update(dt)
-  self.shake.x = util.interpolate(self.shake.x, 0, dt * 12)
-  self.shake.y = util.interpolate(self.shake.y, 0, dt * 12)
+  self.shake.x = util.interpolate(self.shake.x, 0, dt * 10)
+  self.shake.y = util.interpolate(self.shake.y, 0, dt * 10)
 end
 
 function effects:transform(draw)
-  local sx = love.math.random(-self.shake.x, self.shake.x)
-  local sy = love.math.random(-self.shake.y, self.shake.y)
+  local sx = (love.math.random() * 2 - 1) * self.shake.x * 12
+  local sy = (love.math.random() * 2 - 1) * self.shake.y * 12
 
   love.graphics.push()
   love.graphics.translate(sx, sy)
